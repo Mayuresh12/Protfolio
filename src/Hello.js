ea544a36c22032a4099e6426985df9a6a61f793b
Mayuresh Rao <mayureshvrao@gmail.com>
2018-03-11 20:55:08 -0700

    This time works

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return <div>Hello {"Mayuresh"}
+    		<div> 
+    		<button class="button button1">2px</button><button class="button button1">2px</button><button class="button button1">2px</button><button class="button button1">2px</button><button class="button button1">2px</button>
+ </div>
+
+    </div>;
+  }
+}
+
+
+
+export default Hello
